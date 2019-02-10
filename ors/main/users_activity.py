8ec714f328f17e3c6e256a4c3547727d6e190328
ellsharp <ellsharp@aurea-vulpes.com>
2019-02-10 17:59:35 +0900

    Fixed issue #1.

--- ors/main/users_activity.py
+++ ors/main/users_activity.py
@@ -48,19 +48,23 @@ class UsersActivity(object):
             transact_score_score_id = transact_score['score_id']
             transact_score_user_id = transact_score['user_id']
             ranking = 1
-            for leaderboard_score in leaderboard_scores:
-                # Get score's ranking.
-                leaderboard_score_score = leaderboard_score['score']
-                leaderboard_score_score_id = leaderboard_score['id']
-                leaderboard_score_user_id = leaderboard_score['user']['id']
-                if leaderboard_score_score_id == transact_score_score_id:
-                    break
-                elif leaderboard_score_user_id == transact_score_user_id:
-                    pass
-                elif transact_score_score > leaderboard_score_score:
-                    break
-                else:
-                    ranking = ranking + 1
+            # Fixed issue #1
+            if (leaderboard_scores != None):
+                for leaderboard_score in leaderboard_scores:
+                    # Get score's ranking.
+                    leaderboard_score_score = leaderboard_score['score']
+                    leaderboard_score_score_id = leaderboard_score['id']
+                    leaderboard_score_user_id = leaderboard_score['user']['id']
+                    if leaderboard_score_score_id == transact_score_score_id:
+                        break
+                    elif leaderboard_score_user_id == transact_score_user_id:
+                        pass
+                    elif transact_score_score > leaderboard_score_score:
+                        break
+                    else:
+                        ranking = ranking + 1
+            else:
+                ranking = -1
             # Set users activity.
             beatmap_md5 = transact_score['beatmap_md5']
             result = database.execute_statement(connection, 't_users_activity_S03', beatmap_md5)
